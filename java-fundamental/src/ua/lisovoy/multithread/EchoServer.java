3495a64ef23b9697c39f33dcd6e4341395ac2490
vladimir <lisovoy.vladimir@gmail.com>
2016-12-09 10:59:22 +0200

    add echo server

--- java-fundamental/src/ua/lisovoy/multithread/EchoServer.java
+++ java-fundamental/src/ua/lisovoy/multithread/EchoServer.java
@@ -8,13 +8,17 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class EchServer {
+public class EchoServer {
 
     public static void main(String[] args) {
         ServerSocket serverSocket = null;
         List<Socket> socketList = Collections.synchronizedList(new ArrayList<Socket>());
         try {
             serverSocket = new ServerSocket(3000);
+            EchoServerTask serverTask = new EchoServerTask(socketList);
+            Thread thread = new Thread(serverTask);
+            thread.start();
+            System.out.println("Server started...");
 
             while (true) {
                 Socket socket = serverSocket.accept();
@@ -22,6 +26,13 @@ public class EchServer {
             }
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                serverSocket.close();
+                System.out.println("Server finished.");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
--- java-fundamental/src/ua/lisovoy/multithread/EchoServerTask.java
+++ java-fundamental/src/ua/lisovoy/multithread/EchoServerTask.java
@@ -1,10 +1,11 @@
 package ua.lisovoy.multithread;
 
+import java.io.*;
 import java.net.Socket;
 import java.util.List;
 
 /**
- * Created by dp-ptcstd-1 on 12/8/2016.
+ * Created by vladimir on 12/8/2016.
  */
 public class EchoServerTask implements Runnable {
 
@@ -16,6 +17,30 @@ public class EchoServerTask implements Runnable {
 
     @Override
     public void run() {
+        while (true) {
+
+            for (int i = 0; i < socketList.size(); i++) {
+                try {
+                    if (socketList.get(i).getInputStream().available() > 0) {
+
+                        BufferedReader bufferedReader = new BufferedReader(
+                                new InputStreamReader(socketList.get(i).getInputStream()));
+
+                        if (bufferedReader.ready()) {
+                            String line = bufferedReader.readLine();
+                            BufferedWriter bufferedWriter = new BufferedWriter(
+                                    new OutputStreamWriter(socketList.get(i).getOutputStream()));
+                            bufferedWriter.write(line);
+                            bufferedWriter.newLine();
+                            bufferedWriter.flush();
+                        }
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
     }
 }
