d09eee15fbc7699aa7aa7224de88723fe3e4c785
vladimir <lisovoy.vladimir@gmail.com>
2016-12-12 12:13:54 +0200

    add module giggle db

---
+++ giggle-db/src/ua/lisovoy/giggle/db/App.java
@@ -0,0 +1,10 @@
+package ua.lisovoy.giggle.db;
+
+/**
+ * Created by vladimir on 12.12.16.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("work");
+    }
+}
